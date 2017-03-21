2cf8a2b1dcc03ec939f2924c235b79486cf434ad
lcs <lcs>
2017-03-21 18:53:59 +0100

    Improve food controller

--- backend-application/food/rest-api/src/main/java/hu/lamsoft/hms/food/contoller/FoodController.java
+++ backend-application/food/rest-api/src/main/java/hu/lamsoft/hms/food/contoller/FoodController.java
@@ -1,13 +1,14 @@
 package hu.lamsoft.hms.food.contoller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import hu.lamsoft.hms.common.service.food.FoodService;
+import hu.lamsoft.hms.common.service.food.vo.FoodSearchVO;
 import hu.lamsoft.hms.common.service.food.vo.FoodVO;
 
 @RestController
@@ -17,8 +18,8 @@ public class FoodController {
 	private FoodService foodService;
 	
 	@RequestMapping(value = "/foods", method = RequestMethod.GET)
-    public List<FoodVO> getFoods() {
-        return foodService.searchFood(null);
+    public Page<FoodVO> getFoods(@RequestParam(name = "page", required = false, defaultValue = "0") int page, @RequestParam(name = "size", required = false, defaultValue = "10") int size) {
+        return foodService.searchFood(new FoodSearchVO(page, size));
     }
 	
 }
