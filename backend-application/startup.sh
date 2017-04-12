fa785e16a42cc67e2eb1cd8f40dbc0554f70b188
lcs <lcs>
2017-04-12 14:27:54 +0200

    Create startup script

---
+++ backend-application/startup.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+echo "HMS microservices are starting..."
+
+echo "Start eureka server."
+cd ./eureka
+nohup java -jar eureka-1.0-SNAPSHOT.jar &
+sleep 30
+echo "Eureka server started."
+
+echo "Start customer server."
+cd ../customer
+nohup java -jar customer-rest-api-1.0-SNAPSHOT.jar &
+sleep 30
+echo "Customer server started."
+
+echo "Start food server."
+cd ../food
+nohup java -jar food-rest-api-1.0-SNAPSHOT.jar -Dflyway.migration.enabled=false &
+echo "Food server started."
+
+echo "Start nutritionist server."
+cd ../nutritionist
+nohup java -jar nutritionist-rest-api-1.0-SNAPSHOT.jar -Dflyway.migration.enabled=false &
+echo "Nutritionist server started."
+
+echo "Start regimen server."
+cd ../regimen
+nohup java -jar regimen-rest-api-1.0-SNAPSHOT.jar -Dflyway.migration.enabled=false &
+echo "Regimen server started."
+
+echo "Start Zuul server."
+cd ../zuul
+nohup java -jar zuul-1.0-SNAPSHOT.jar &
+echo "Zuul server started."
