8f509dd2f531239ae03702e8ba4588a7ca218534
lcs <lcs>
2017-04-12 19:30:31 +0200

    Test environment preparation

--- backend-application/common/rest-api/src/main/java/hu/lamsoft/hms/common/restapi/security/jwt/TokenAuthenticationService.java
+++ backend-application/common/rest-api/src/main/java/hu/lamsoft/hms/common/restapi/security/jwt/TokenAuthenticationService.java
@@ -5,6 +5,9 @@ import java.util.Date;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 
 import io.jsonwebtoken.Jwts;
@@ -12,6 +15,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 
 public class TokenAuthenticationService {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(TokenAuthenticationService.class);
 	private static final long EXPIRATIONTIME = 1000 * 60 * 60 * 4; // 4 hour
     private static final String SECRET = "ThisIsASecret";
     private static final String TOKEN_PREFIX = "Bearer";
@@ -28,17 +32,27 @@ public class TokenAuthenticationService {
 
     public Authentication getAuthentication(HttpServletRequest request) {
         String token = request.getHeader(HEADER_STRING);
-        if(token != null) {
-            String username = Jwts.parser()
-				.setSigningKey(SECRET)
-				.parseClaimsJws(token)
-				.getBody()
-				.getSubject();
-            if(username != null) {
-                return new AuthenticatedUser(username);
-            }
+        if(StringUtils.isNotBlank(token)) {
+        	try {
+        		return getAuthenticatedUser(token);
+        	} catch (Exception e) {
+        		LOGGER.warn("Exception while get authentication.", e);
+        	}
         }
         return null;
     }
 	
+    private AuthenticatedUser getAuthenticatedUser(String token) {
+    	String username = Jwts.parser()
+			.setSigningKey(SECRET)
+			.parseClaimsJws(token)
+			.getBody()
+			.getSubject();
+        if(username != null) {
+            return new AuthenticatedUser(username);
+        } else {
+        	return null;
+        }
+    }
+    
 }
--- backend-application/startup.sh
+++ backend-application/startup.sh
@@ -16,19 +16,23 @@ echo "Customer server started."
 echo "Start food server."
 cd ../food
 nohup java -jar food-rest-api-1.0-SNAPSHOT.jar -Dflyway.migration.enabled=false &
+sleep 15
 echo "Food server started."
 
 echo "Start nutritionist server."
 cd ../nutritionist
 nohup java -jar nutritionist-rest-api-1.0-SNAPSHOT.jar -Dflyway.migration.enabled=false &
+sleep 15
 echo "Nutritionist server started."
 
 echo "Start regimen server."
 cd ../regimen
 nohup java -jar regimen-rest-api-1.0-SNAPSHOT.jar -Dflyway.migration.enabled=false &
+sleep 15
 echo "Regimen server started."
 
 echo "Start Zuul server."
 cd ../zuul
 nohup java -jar zuul-1.0-SNAPSHOT.jar &
+sleep 10
 echo "Zuul server started."
